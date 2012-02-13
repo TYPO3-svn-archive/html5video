a176709f7864e5923bbaa47d8a064a263c6c8d84
gordon81 <gordon81@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-13 23:41:36 +0000

    new Version 2.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/html5video/trunk@57646 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_html5video_pi1.php
+++ pi1/class.tx_html5video_pi1.php
@@ -1,29 +1,25 @@
 <?php
 /***************************************************************
  *  Copyright notice
+ *  (c) 2010 Gordon Brüggemann <gb
  *
- *  (c) 2010 Gordon Brüggemann <gb@gb-web.de>
+ * @gb-web.de>
  *  All rights reserved
- *
  *  This script is part of the TYPO3 project. The TYPO3 project is
  *  free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
- *
  *  The GNU General Public License can be found at
  *  http://www.gnu.org/copyleft/gpl.html.
- *
  *  This script is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-
-require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once(PATH_tslib . 'class.tslib_pibase.php');
 
 /**
  * Plugin 'HTML5 Video Player' for the 'html5video' extension.
@@ -33,10 +29,18 @@ require_once(PATH_tslib.'class.tslib_pibase.php');
  * @subpackage	tx_html5video
  */
 class tx_html5video_pi1 extends tslib_pibase {
-	var $prefixId  = 'tx_html5video_pi1';		// Same as class name
-	var $scriptRelPath = 'pi1/class.tx_html5video_pi1.php';	// Path to this script relative to the extension dir.
-	var $extKey= 'html5video';	// The extension key.
+
+	var $prefixId = 'tx_html5video_pi1'; // Same as class name
+	var $scriptRelPath = 'pi1/class.tx_html5video_pi1.php'; // Path to this script relative to the extension dir.
+	var $extKey = 'html5video'; // The extension key.
 	var $pi_checkCHash = true;
+
+	public $conf = array();
+	private $data= array();
+	private $poster = array();
+	private $download ='';
+	private $baseURL='';
+
 
 	/**
 	 * The main method of the PlugIn
@@ -47,7 +51,8 @@ class tx_html5video_pi1 extends tslib_pibase {
 	 */
 	function main($content, $conf) {
 		$this->conf = $conf;
-		if(!is_array($conf['type.'])){
+		$this->data = 	$this->cObj->data;
+		if ( !is_array($conf['type.']) ) {
 			$this->conf['type.'] = array(
 				'flv' => 'video/x-flv',
 				'mp4' => 'video/mp4',
@@ -63,68 +68,67 @@ class tx_html5video_pi1 extends tslib_pibase {
 		$this->getFileData();
 		$this->getHeader();
 		$this->getPoster();
+		$this->baseURL = $GLOBALS['TSFE']->tmpl->setup['config.']['baseURL'];
+		$support = $this->conf['SupportVideoJS'] ? $this->cObj->cObjGetSingle($this->conf['support'], $this->conf['support.']) : '';
+		$content = $this->cObj->stdWrap($this->getVideo() . $this->getDownload(), $this->conf['video.']);
 
-		$this->baseURL=$GLOBALS['TSFE']->tmpl->setup['config.']['baseURL'];
-		$support=$this->conf['SupportVideoJS']?$this->cObj->cObjGetSingle($this->conf['support'],$this->conf['support.']):'';
-		$content = $this->cObj->stdWrap($this->getVideo().$this->getDownload() , $this->conf['video.']);
-
-		return $this->pi_wrapInBaseClass($content.$support);
+		return $this->pi_wrapInBaseClass($content . $support);
 	}
 
 	/**
 	 * give the video tag
+	 *
 	 * @return The video tag
 	 */
-	public function getVideo(){
-		if(trim($this->conf['skin'])!=='orginal'){
-			$class['video'] = empty($this->conf['video.']['class'])?' class="'.$this->conf['skin'].'" ':' class="'.$this->conf['video.']['class'].' '.$this->conf['skin'].'" ';
-		}else{
-			$class['video'] = empty($this->conf['video.']['class'])?' ':' class="'.$this->conf['video.']['class'].'"';
+	public function getVideo() {
+		if ( trim($this->conf['skin']) !== 'orginal' ) {
+			$class['video'] = empty($this->conf['video.']['class']) ? ' class="' . $this->conf['skin'] . '" ' : ' class="' . $this->conf['video.']['class'] . ' ' . $this->conf['skin'] . '" ';
+		} else {
+			$class['video'] = empty($this->conf['video.']['class']) ? ' ' : ' class="' . $this->conf['video.']['class'] . '"';
 		}
-		$id['video'] = empty($this->conf['video.']['id'])?' ':' id="'.$this->conf['video.']['id'].'"';
+		$id['video'] = empty($this->conf['video.']['id']) ? 'v' . $this->data['uid'] : ' id="' . $this->conf['video.']['id'] . '-' . $this->data['uid']  . '"';
 		$setup = ' data-setup=\'{}\' ';
-		$preload= $this->conf['PreloadVideo']?' preload="true" ':'';
-		$autoplay=$this->conf['AutoplayVideo']?' autoplay="true" ':'';
-		$source ='';
-	foreach($this->conf['source.'] as $type => $file){
-		if(is_file($file)){
-			//source file für Video
-			if($type != 'poster'and $type != 'flv'){
-				$sourc['src']= 'src="'.$this->baseURL.$file.'"';
-				$sourctype='type=\''.$this->conf['type.'][$type].'\'';
-				$source.='<source '.$sourc['src'].$sourctype.' /> ';
-			}
-			//download für die Filme bei error
-			if($type != 'poster'){
-				$this->download.='<a href="'.$file.'">'.$this->conf['download.'][$type].'</a>';  //TYPO3 link verwenden
+		$preload = $this->conf['PreloadVideo'] ? ' preload="true" ' : '';
+		$autoplay = $this->conf['AutoplayVideo'] ? ' autoplay="true" ' : '';
+		$source = '';
+		foreach ( $this->conf['source.'] as $type => $file ) {
+			if ( is_file($file) ) {
+				//source file für Video
+				if ( $type != 'poster'and $type != 'flv' ) {
+					$sourc['src'] = 'src="' . $this->baseURL . $file . '"';
+					$sourctype = 'type=\'' . $this->conf['type.'][$type] . '\'';
+					$source .= '<source ' . $sourc['src'] . $sourctype . ' /> ';
+				}
+				//download für die Filme bei error
+				if ( $type != 'poster' ) {
+					$this->download .= '<a href="' . $file . '">' . $this->conf['download.'][$type] . '</a>'; //TYPO3 link verwenden
+				}
 			}
 		}
-	}
-		$video='<video'.$id['video'].' ' . $class['video'] . 'width="' . $this->conf['width'] . '" height="' . $this->conf['height'] . '" controls="" ' . $preload.$autoplay.$setup.' '.$this->poster['video'] . '>';
-		$video.= $source;
-		$video.='</video>';
+		$video = '<video' . $id['video'] . ' ' . $class['video'] . 'width="' . $this->conf['width'] . '" height="' . $this->conf['height'] . '" controls="" ' . $preload . $autoplay . $setup . ' ' . $this->poster['video'] . '>';
+		$video .= $source;
+		$video .= '</video>';
 		return $video;
 	}
 
-
 	/**
 	 * creat the image for the player
-	 *
+
 	 */
-	public function getPoster(){
-		$this->poster['video']='';
-		if(!empty($this->conf['source.']['poster'])){
-			$this->poster['alt']= empty($this->conf['poster.']['alt'])?'':' alt="'.$this->conf['poster.']['alt'].'"';
-			$this->poster['title']= empty($this->conf['poster.']['titel'])?'':' title="'.$this->conf['poster.']['titel'].'"';
-			$this->poster['video']= ' poster="'.$this->conf['source.']['poster'].'"';
+	public function getPoster() {
+		$this->poster['video'] = '';
+		if ( !empty($this->conf['source.']['poster']) ) {
+			$this->poster['alt'] = empty($this->conf['poster.']['alt']) ? '' : ' alt="' . $this->conf['poster.']['alt'] . '"';
+			$this->poster['title'] = empty($this->conf['poster.']['titel']) ? '' : ' title="' . $this->conf['poster.']['titel'] . '"';
+			$this->poster['video'] = ' poster="' . $this->conf['source.']['poster'] . '"';
 		}
 	}
 
 	/**
 	 * @return The downloads for the video
 	 */
-	public function getDownload(){
-		$download=$this->conf['download']?$this->cObj->stdWrap($this->download, $this->conf['download.']):'';
+	public function getDownload() {
+		$download = $this->conf['download'] ? $this->cObj->stdWrap($this->download, $this->conf['download.']) : '';
 
 		return $download;
 	}
@@ -132,86 +136,75 @@ class tx_html5video_pi1 extends tslib_pibase {
 	/**
 	 * creat the header information
 	 */
-	public function getHeader(){
-		$jsOptions='';
-		$jsOptions=$this->jsOptions();
- 	$this->conf['options'] = empty($this->conf['options'])?'myVideoJSPlayers':trim($this->conf['options']);
+	public function getHeader() {
 		//JS File
-		$videoJS = empty($this->conf['videoJS'])?'typo3conf/ext/html5video/res/videoJS/video.min.js':$this->conf['videoJS'];
-		$cdnURLjs = empty($this->conf['cdnURLjs'])?'http://vjs.zencdn.net/c/video.js':$this->conf['cdnURLjs'];
+		$videoJS = empty($this->conf['videoJS']) ? 'typo3conf/ext/html5video/res/videoJS/video.min.js' : $this->conf['videoJS'];
+		$cdnURLjs = empty($this->conf['cdn.']['js']) ? $videoJS : $this->conf['cdn.']['js'];
 		//CSS File
-		$videoCSS = empty($this->conf['videoCSS'])?'typo3conf/ext/html5video/res/videoJS/video-js.min.css':$this->conf['videoCSS'];
-		$cdnURLcss = empty($this->conf['cdnURLcss'])?'http://vjs.zencdn.net/c/video-js.css':$this->conf['cdnURLcss'];
+		$videoCSS = empty($this->conf['videoCSS']) ? 'typo3conf/ext/html5video/res/videoJS/video-js.min.css' : $this->conf['videoCSS'];
+		$cdnURLcss = empty($this->conf['cdn.']['css']) ? $videoCSS : $this->conf['cdn.']['css'];
 		// include with CDN or none
-		if( $this->conf['useCDN']){
-			$cdnCssJs = '<link href="'.$cdnURLcss.'" rel="stylesheet">
-						 <script src="'.$cdnURLjs.'"></script><script>window.VideoJS || document.write(\'<script src="'.$videoJS.'"><\/script><link rel="stylesheet" href="'.$videoCSS.'" type="text/css" media="screen" >\')</script>';
-		}else{
+		if ( $this->conf['cdn.']['enable'] ) {
+			$cdnCssJs = '<link href="' . $cdnURLcss . '" rel="stylesheet">
+						 <script src="' . $cdnURLjs . '"></script><script>window.VideoJS || document.write(\'<script src="' . $videoJS . '"><\/script><link rel="stylesheet" href="' . $videoCSS . '" type="text/css" media="screen" >\')</script>';
+		} else {
 			$GLOBALS['TSFE']->pSetup['includeCSS.'][$this->extKey] = $videoCSS;
 			$GLOBALS['TSFE']->pSetup['includeJS.'][$this->extKey] = $videoJS;
 		}
 		//extra CSS File für wechselnde Skin
-	$skinCSS='';
-		if( !empty($this->conf['skin'])){
-			if(trim($this->conf['skin'])!=='orginal'){
-				$skinCSS = '<link rel="stylesheet" href="'.$this->conf['skin.'][$this->conf['skin']].'" type="text/css" media="screen" title="Video '.$this->conf['skin'].'" charset="utf-8">';
+		$skinCSS = '';
+		if ( !empty($this->conf['skin']) ) {
+			if ( trim($this->conf['skin']) !== 'orginal' ) {
+				$skinCSS = '<link rel="stylesheet" href="' . $this->conf['skin.'][$this->conf['skin']] . '" type="text/css" media="screen" title="Video ' . $this->conf['skin'] . '" charset="utf-8">';
 			}
 		}
-		//start script
-		$setupJS ='';
-		if( empty($this->conf['setupJS'])){
-			$setupJS ='<script type="text/javascript" >  VideoJS.DOMReady(function(){ var '.$this->conf['options'].' = VideoJS.setup("All"'.$jsOptions.');
-}); </script>';
-		}else{
-			// wenn gesetzt auch keine jsOptions
-			$setupJS = $this->conf['setupJS'];
-		}
-		$GLOBALS['TSFE']->additionalHeaderData[$this->prefixId] = $cdnCssJs.$skinCSS;
+		$GLOBALS['TSFE']->additionalHeaderData[$this->prefixId] = $cdnCssJs . $skinCSS;
 	}
+
 	/**
-	 *
 	 *creat JavaScript Options
-	 *
+
 	 */
-	public function jsOptions(){
-		$jsOptions='';
-		if(is_array($this->conf['options.'])){
- 	//$jsOptions = $this->conf['options'];
- 	$jsOptions.= ' ,{';
- 	 foreach($this->conf['options.'] as $option => $value){
- 	 	$jsOptions.= empty($value)?'':$option.':'.$value.',';
- 	 }
- $jsOptions = substr($jsOptions,0,-1);
- 	 $jsOptions.= '}';
- 	return $jsOptions;
- 	}
- 	return;
+	public function jsOptions() {
+		$jsOptions = '';
+		if ( is_array($this->conf['options.']) ) {
+			//$jsOptions = $this->conf['options'];
+			$jsOptions .= ' ,{';
+			foreach ( $this->conf['options.'] as $option => $value ) {
+				$jsOptions .= empty($value) ? '' : $option . ':' . $value . ',';
+			}
+			$jsOptions = substr($jsOptions, 0, -1);
+			$jsOptions .= '}';
+			return $jsOptions;
+		}
+		return;
 	}
+
 	/**
 	 * creat the patch for the videos dam and from upload folder
 	 */
-	public function getFileData(){
+	public function getFileData() {
 		$row = $this->cObj->data;
-		if (isset($row['_ORIG_uid']) && ($row['_ORIG_uid'] > 0)) {
-	  $uid = $row['_ORIG_uid'];
-		}else{
-		  $uid = $row['uid'];
+		if ( isset($row['_ORIG_uid']) && ($row['_ORIG_uid'] > 0) ) {
+			$uid = $row['_ORIG_uid'];
+		} else {
+			$uid = $row['uid'];
 		}
-		if ($row['_LOCALIZED_UID']) {
-		  $uid = $row['_LOCALIZED_UID'];
+		if ( $row['_LOCALIZED_UID'] ) {
+			$uid = $row['_LOCALIZED_UID'];
 		}
-		$tableofcontent='tt_content';
-		foreach($this->conf['source.'] as $key => $value){
-			if(t3lib_extMgm::isLoaded('dam')){
-				$damArray[$key]= tx_dam_db::getReferencedFiles($tableofcontent,  $uid, 'html5video'.$key);
-				foreach ($damArray[$key]['files'] as $id => $files){
+		$tableofcontent = 'tt_content';
+		foreach ( $this->conf['source.'] as $key => $value ) {
+			if ( t3lib_extMgm::isLoaded('dam') ) {
+				$damArray[$key] = tx_dam_db::getReferencedFiles($tableofcontent, $uid, 'html5video' . $key);
+				foreach ( $damArray[$key]['files'] as $id => $files ) {
 					$this->conf['source.'][$key] = $files;
-					$this->conf['source.'][$key.'.']['rows']=$damArray[$key][$id];
+					$this->conf['source.'][$key . '.']['rows'] = $damArray[$key][$id];
 				}
-			}else{
-				$this->conf['patch']=empty($this->conf['patch'])?'uploads/html5video/':$this->conf['patch'];
-				if(is_file( $this->conf['patch'].$this->conf['source.'][$key])){
-					$this->conf['source.'][$key]= $this->conf['patch'].$this->conf['source.'][$key];
+			} else {
+				$this->conf['patch'] = empty($this->conf['patch']) ? 'uploads/html5video/' : $this->conf['patch'];
+				if ( is_file($this->conf['patch'] . $this->conf['source.'][$key]) ) {
+					$this->conf['source.'][$key] = $this->conf['patch'] . $this->conf['source.'][$key];
 				}
 			}
 		}
@@ -219,22 +212,24 @@ class tx_html5video_pi1 extends tslib_pibase {
 
 	/**
 	 * write the flex daten in $this->conf
+	 *
+	 * @author	unbekannt
 	 */
 	public function flex2conf(&$pObj) {
-		if (is_array($pObj->cObj->data['pi_flexform']['data'])) { // if there are flexform values
-			foreach ($pObj->cObj->data['pi_flexform']['data'] as $key => $value) { // every flexform category
-				if (is_array($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'])) {
-					foreach ($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'] as $key2 => $value2) { // every flexform option
-						if (is_array($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2])) {
-							foreach($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2] as $key3 => $value3){
-								if( $key3==="el"){
-									foreach($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2][$key3] as $key4 => $value4){
-										if(!empty($value4['vDEF']) AND $value4['vDEF']!='0'){
-											$pObj->conf[$key2.'.'][$key4] = $value4['vDEF'];
+		if ( is_array($pObj->cObj->data['pi_flexform']['data']) ) { // if there are flexform values
+			foreach ( $pObj->cObj->data['pi_flexform']['data'] as $key => $value ) { // every flexform category
+				if ( is_array($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF']) ) {
+					foreach ( $pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'] as $key2 => $value2 ) { // every flexform option
+						if ( is_array($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2]) ) {
+							foreach ( $pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2] as $key3 => $value3 ) {
+								if ( $key3 === "el" ) {
+									foreach ( $pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2][$key3] as $key4 => $value4 ) {
+										if ( !empty($value4['vDEF']) AND $value4['vDEF'] != '0' ) {
+											$pObj->conf[$key2 . '.'][$key4] = $value4['vDEF'];
 										}
 									}
-								}else{
-									if(!empty($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2]['vDEF'])){
+								} else {
+									if ( !empty($pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2]['vDEF']) ) {
 										$pObj->conf[$key2] = $pObj->cObj->data['pi_flexform']['data'][$key]['lDEF'][$key2]['vDEF'];
 									}
 								}
@@ -245,13 +240,9 @@ class tx_html5video_pi1 extends tslib_pibase {
 			}
 		}
 	}
-
-
-
-
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/html5video/pi1/class.tx_html5video_pi1.php'])	{
+if ( defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/html5video/pi1/class.tx_html5video_pi1.php'] ) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/html5video/pi1/class.tx_html5video_pi1.php']);
 }
 ?>
